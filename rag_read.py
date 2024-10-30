9a94e34cbb4600f5fb226cf1446a002d1737baf7
Puneet Joshi <puneet@ip-172-29-9-82.ap-south-1.compute.internal>
2024-10-30 01:21:51 +0000

    Updated.

---
+++ rag_read.py
@@ -0,0 +1,17 @@
+from langchain_huggingface import HuggingFaceEmbeddings
+from langchain_community.docstore.in_memory import InMemoryDocstore
+from langchain_community.vectorstores import FAISS
+from langchain_community.document_loaders import PyPDFLoader
+from langchain_community.document_loaders import PyMuPDFLoader
+
+embeddings = HuggingFaceEmbeddings(model_name="all-MiniLM-L6-v2")
+
+query = "As a student do I get any benefit?"
+vector_store = FAISS.load_local(
+    "../faiss/programs_index", embeddings, allow_dangerous_deserialization=True
+)
+
+query="Are there any schemes related to student?"
+docs = vector_store.similarity_search(query)
+print(docs[0])
+
--- rag_write.py
+++ rag_write.py
@@ -8,11 +8,6 @@ loader = PyMuPDFLoader('../docs/social-benefit-programs.pdf')
 documents = loader.load()
 
 embeddings = HuggingFaceEmbeddings(model_name="all-MiniLM-L6-v2")
-#embeddings = HuggingFaceEmbeddings(model_name="all-mpnet-base-v2")
 
 vector_store = FAISS.from_documents(documents, embeddings)
-
-query = "As a student do I get any benefit?"
-results = vector_store.similarity_search(query)
-for result in results:
-    print(result.page_content)
+vector_store.save_local("programs_index")
