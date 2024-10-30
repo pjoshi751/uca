abb01105dd6d8b92e81af11e257ed8d152514207
Puneet Joshi <puneet@ip-172-29-9-82.ap-south-1.compute.internal>
2024-10-30 06:49:11 +0000

    Splitting added.

--- rag_write.py
+++ rag_write.py
@@ -1,13 +1,16 @@
 from langchain_huggingface import HuggingFaceEmbeddings
 from langchain_community.docstore.in_memory import InMemoryDocstore
 from langchain_community.vectorstores import FAISS
-from langchain_community.document_loaders import PyPDFLoader
 from langchain_community.document_loaders import PyMuPDFLoader
+from langchain_text_splitters import RecursiveCharacterTextSplitter
 
 loader = PyMuPDFLoader('../docs/social-benefit-programs.pdf')
-documents = loader.load()
+docs = loader.load()
 
+text_splitter = RecursiveCharacterTextSplitter(
+    chunk_size=1000, chunk_overlap=200, add_start_index=True
+)
+splits = text_splitter.split_documents(docs)
 embeddings = HuggingFaceEmbeddings(model_name="all-MiniLM-L6-v2")
-
-vector_store = FAISS.from_documents(documents, embeddings)
-vector_store.save_local("programs_index")
+vector_store = FAISS.from_documents(splits, embeddings)
+vector_store.save_local("../faiss/programs_index")
